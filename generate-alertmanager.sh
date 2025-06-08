4e0f7659a95fd3810bd5cde1258dc039ebc833e0
Fabien <lapertfabien@gmail.com>
2025-06-08 20:32:41 +0200

    update gitignore and add alertmanager

--- generate-alertmanager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Chargement des variables d'environnement
-source .env
-
-# Génération du fichier alertmanager.yml avec substitution des variables
-envsubst < monitoring/alertmanager.yml.template > monitoring/alertmanager.yml
-
-echo "✅ Fichier alertmanager.yml généré avec les variables d'environnement"
